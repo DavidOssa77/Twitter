3e1db70045da5f1527d783d105ca0076ab7f1980
David <davidossa45@gmail.com>
2026-05-20 10:56:54 -0500

    Excepción ContenidoNoEncontrado

---
+++ src/main/java/com/twitter/exception/ContenidoNoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.twitter.exception;
+
+public class ContenidoNoEncontradoException extends RuntimeException {
+
+    public ContenidoNoEncontradoException(String message) {
+        super(message);
+    }
+}
