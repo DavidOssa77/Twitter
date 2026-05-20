fe3f9ecdc7f20379f8390fee209edd0d5c2150c0
Tetoagudelo <teomagudeloa@gmail.com>
2026-05-20 17:53:32 -0500

    Clase like

---
+++ src/main/java/com/twitter/model/Like/Like.java
@@ -0,0 +1,35 @@
+package com.twitter.model.Like;
+
+import com.twitter.model.Interaction.Interaction;
+import com.twitter.model.tweet.Tweet;
+import com.twitter.model.user.Usuario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Like extends Interaction {
+    private static final Logger logger = LoggerFactory.getLogger(Like.class);
+
+    private Tweet tweetObjetivo;
+
+    public Like(String id, Usuario emisor, Tweet tweetObjetivo) {
+        super(id, emisor);
+        this.tweetObjetivo = tweetObjetivo;
+
+    }
+
+    @Override
+    public void ejecutar() {
+        logger.info("@{} dio Like al tweet {}",
+                getEmisor().getAlias(),
+                tweetObjetivo.getId());
+    }
+
+    @Override
+    public String getTipo() {
+        return "Like";
+    }
+
+    public Tweet getTweetObjetivo() {
+        return tweetObjetivo;
+    }
+}
