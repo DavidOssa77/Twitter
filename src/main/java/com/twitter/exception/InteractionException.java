c7a9a9f7396c0dc28fc29a6c7c0af90f56cb9289
Tetoagudelo <teomagudeloa@gmail.com>
2026-05-20 17:50:09 -0500

    Excepcion personalizada para Interaction

---
+++ src/main/java/com/twitter/exception/InteractionException.java
@@ -0,0 +1,8 @@
+package com.twitter.exception;
+
+public class InteractionException extends RuntimeException {
+
+    public InteractionException(String message) {
+        super(message);
+    }
+}
